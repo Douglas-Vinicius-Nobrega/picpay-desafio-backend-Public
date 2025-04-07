5153e144309b40885f6f6319b76f079c83466230
Douglas Nobrega <dougrete2014@outlook.com>
2025-04-07 17:48:46 -0300

    entidades iniciais

---
+++ src/main/java/br/com/desafio/picpay/domain/enums/UserType.java
@@ -0,0 +1,5 @@
+package br.com.desafio.picpay.domain.enums;
+
+public enum UserType {
+    COMUM, LOJISTA
+}
---
+++ src/main/java/br/com/desafio/picpay/domain/model/User.java
@@ -0,0 +1,37 @@
+package br.com.desafio.picpay.domain.model;
+
+import br.com.desafio.picpay.domain.enums.UserType;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome_completo", nullable = false)
+    private String nomeCompleto;
+
+    @Column(nullable = false, unique = true, length = 14)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "user_type", nullable = false)
+    private UserType userType;
+}
